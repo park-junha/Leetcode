b1e234286c3bdbf0722e800e6b526eb620d074fc
Junha Park <jpark3@scu.edu>
2020-05-08 11:17:56 -0700

    Q2 Solution 1 Java + ListNode

--- 0002-AddTwoNumbers/Solution.java
+++ 0002-AddTwoNumbers/Solution.java
@@ -5,6 +5,17 @@
     Note: n is the length of the larger of the two lists.
  */
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode sum = new ListNode();
