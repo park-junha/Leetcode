a3c5b56b126589563ed3d40a9f8f7e8d217e80b2
Junha Park <jpark3@scu.edu>
2020-05-22 20:38:29 -0700

    Q11 Solution 2 Go

--- 00xx/001x/0011-ContainerWithMostWater/Solution.go
+++ 00xx/001x/0011-ContainerWithMostWater/Solution.go
@@ -1,21 +1,33 @@
-/*  Solution: Brute Force
-    Time Complexity:    O(n^2)
+/*  Solution: Two Windows
+    Time Complexity:    O(n)
     Space Complexity:   O(1)
+
+    Note: Keep track of two pointers and move the pointer at the shorter
+          edge of the rectangle inwards.
+
+          Moving the pointer of the longer edge will never yield a larger
+          area because the shorter edge won't be altered + we decrease the
+          width of the rectangle each time we move a pointer inwards
  */
 
 func maxArea(height []int) int {
   max := 0
-  for i := 0; i < len(height) - 1; i++ {
-    for j := i + 1; j < len(height); j++ {
-      var h int
-      if height[i] < height[j] {
-        h = height[i]
-      } else {
-        h = height[j]
-      }
+  i := 0
+  j := len(height) - 1
+  for i < j {
+    var h int
+    if height[i] < height[j] {
+      h = height[i]
       if (j - i) * h > max {
         max = (j - i) * h
       }
+      i++
+    } else {
+      h = height[j]
+      if (j - i) * h > max {
+        max = (j - i) * h
+      }
+      j--
     }
   }
   return max
