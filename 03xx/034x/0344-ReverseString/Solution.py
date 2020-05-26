d7501569381053b4c924b3965588b41aef9f04f4
Junha Park <jpark3@scu.edu>
2020-05-26 09:21:26 -0700

    Q344 Solution 1 Python

---
+++ 03xx/034x/0344-ReverseString/Solution.py
@@ -0,0 +1,12 @@
+# Solution: Two Pointers
+# Time Complexity: O(ceil(n/2))
+# Space Complexity: O(1)
+
+class Solution:
+  def reverseString(self, s: List[str]) -> None:
+    i = 0
+    j = len(s) - 1
+    while i < j:
+      s[i], s[j] = s[j], s[i]
+      i += 1
+      j -= 1
